e5223c163ede1749ebeaded14b46840ac89c57d2
Nicholas Galea <nich.galea@gmail.com>
2019-05-10 11:02:08 +0200

    Add scroll into view for playlist entry

--- src/playlist/playlist-entry.tsx
+++ src/playlist/playlist-entry.tsx
@@ -13,15 +13,40 @@ interface Props {
 }
 
 export default class PlayistEntry extends Component<Props> {
+  private element!: HTMLLIElement;
+  private intersectionObserver!: IntersectionObserver;
+  private isVisible = false;
+
   constructor(props) {
     super(props);
 
     this.handleClick = this.handleClick.bind(this);
   }
 
+  componentDidMount() {
+    this.intersectionObserver = new IntersectionObserver(([entry]) => {
+      this.isVisible = entry.intersectionRatio > 0.4;
+    });
+
+    this.intersectionObserver.observe(this.element);
+  }
+
+  componentDidUpdate(prevProps: Readonly<Props>) {
+    // if this entry starts playing and isn't visible
+    if (this.props.nowPlaying && !prevProps.nowPlaying && !this.isVisible) {
+      // scroll it into view
+      this.element.scrollIntoView({ behavior: "smooth", block: "nearest" });
+    }
+  }
+
   render() {
     return (
-      <li title="Click to play" className={cx(this.props.nowPlaying && styles.nowPlaying)} onClick={this.handleClick}>
+      <li
+        title="Click to play"
+        ref={e => (this.element = e!)}
+        className={cx(this.props.nowPlaying && styles.nowPlaying)}
+        onClick={this.handleClick}
+      >
         {this.props.nowPlaying ? <div className={styles.nowPlayingText}>Now Playing</div> : null}
         <div className={styles.songTitle}>{this.props.video.title}</div>
         <div className={styles.artist}>{this.props.video.artist}</div>
